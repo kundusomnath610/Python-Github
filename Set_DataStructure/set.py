742e538e93009ea57c57fcdfb53e4874e00895fa
Somnath Kundu <kundusomnath610@gmail.com>
2025-03-18 19:09:51 +0530

    initial commit

---
+++ Set_DataStructure/set.py
@@ -0,0 +1,15 @@
+s1 = {10, 20, 20, True, 'code'}
+print(s1, type(s1))
+
+s1.add(300)
+print(s1)
+
+'''
+
+    1. Set store homoginius and hetroginius data both.
+    2. Set is unorder collection of data because set genarate the output
+        in random order. It dosen't maintain the order the insertion in output.
+    3. Set can not store duplicate value.
+    4. Set is mutable means onces we declear set we can modified the set DataStructure.
+
+'''
