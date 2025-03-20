ed8bf85623422d4614eb67f9dbed7f6ae8618f3d
Somnath Kundu <kundusomnath610@gmail.com>
2025-03-20 23:17:54 +0530

    Condition operartor

---
+++ Logical.py
@@ -0,0 +1,8 @@
+number1 = int(input("Enter first number:"))
+number2 = int(input("Enter Second number:"))
+
+and_result = number1 & number2 ## And condition
+or_result = number1 | number2 ## OR Condition
+
+print(f"Bitwise and of two number:", and_result) 
+print(f"Bitwise OR result of two number:", or_result)
