4ab9ea30a2afbe84d4fe9b163d4228b9881e85e3
Somnath Kundu <kundusomnath610@gmail.com>
2025-03-25 18:21:00 +0530

    initial commit

---
+++ Obeject_oriented_programming/constructor.py
@@ -0,0 +1,10 @@
+class Employee:
+    def __init__(self): ## Constructor __init__  class Employee
+        self.emp_name = "AK" ## Intance Variable
+        self.emp_id = 101 ## Intance Variable
+## Creating Object
+e1 = Employee()
+e2 = Employee()
+
+print("Employee name:", e1.emp_name)
+print("Employee ID:", e2.emp_id)
