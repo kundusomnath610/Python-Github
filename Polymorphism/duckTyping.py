49d97be9168e309f33ded4ca0e88b386d8d8e290
Somnath Kundu <kundusomnath610@gmail.com>
2025-03-28 19:12:12 +0530

    initial commit

---
+++ Polymorphism/duckTyping.py
@@ -0,0 +1,14 @@
+class Calsi:
+    def calculate(self, a,b):
+        print('calculating a and b')
+
+class Add(Calsi):
+    def calculate(self, a, b):
+        print(a+b)
+
+class Mul(Calsi):
+    def calculate(self, a, b):
+        print(a * b)
+
+class div(Calsi):
+    pass
