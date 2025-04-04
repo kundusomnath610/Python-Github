30f76b100544b2d12dc5b3f115247dc5ccae71ba
Somnath Kundu <kundusomnath610@gmail.com>
2025-04-04 19:17:45 +0530

    commit

--- Multithread/DemonThread.py
+++ Multithread/DemonThread.py
@@ -7,7 +7,7 @@ def background_task():
         time.sleep(1)
 
 # Create a Demon thread
-thread = threading.Thread(target=background_task)
+thread = threading.Thread(target=background_task) ## Threading is a Module and Thread is a Class
 thread.daemon = True # Set a thread as a demon
 thread.start() # Start Method
 
