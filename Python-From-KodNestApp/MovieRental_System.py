91c356bea6a3b022e9591942b79cbd8b585e5157
Somnath Kundu <kundusomnath610@gmail.com>
2025-04-12 13:39:33 +0530

    commit

---
+++ Python-From-KodNestApp/MovieRental_System.py
@@ -0,0 +1,26 @@
+def add_movie(title, available_movies):
+    if title not in available_movies:
+        available_movies.append(title)
+
+def search_movie(title, available_movies):
+    if title in available_movies:
+        print(f"'{title}' is available in the collection.")
+    else:
+        print(f"'{title}', is not available in Movie List")
+
+
+def view_movies(available_movies):
+    print(f"Available movies: {available_movies}")
+
+available_movies = ["The Matrix", "Interstellar", "The Godfather"]
+
+view_movies(available_movies)
+
+add_movie('Inception', available_movies)
+
+view_movies(available_movies)
+
+search_movie('The Matrix', available_movies)
+
+search_movie('Godfather', available_movies)
+    
