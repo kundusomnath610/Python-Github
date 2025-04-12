5c76b9e767ab337a17b0d0246dcedb334fa30087
Somnath Kundu <kundusomnath610@gmail.com>
2025-04-12 16:06:03 +0530

    commit

--- Python-From-KodNestApp/Discount_Eligibility.py
+++ Python-From-KodNestApp/Discount_Eligibility.py
@@ -1,6 +1,12 @@
-purchase_amount = 100
+score = int(input("Enter a score: "))
 
-if purchase_amount >= 100:
-    print("Congratulation !!")
-else:
-    print("No")
+if score >= 90:
+    print("Grade: A")
+elif score >= 80 and score <= 89:
+    print("Grade: B")
+elif score >= 70 and score <= 79:
+    print("Grade: C")
+elif score >= 60 and score <= 69:
+    print("Grade: D")
+elif score <= 60:
+    print("Grade: F")
