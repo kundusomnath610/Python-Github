d6ebb7b7978b178bb08d584398a2685b5b143275
Somnath Kundu <kundusomnath610@gmail.com>
2025-04-16 00:08:03 +0530

    commit

--- Pattern Program/PrymidProgram.py
+++ Pattern Program/PrymidProgram.py
@@ -2,5 +2,5 @@ rows = int(input("Enter a number:- "))
 
 for i in range(1, rows + 1):
     for j in range(i):
-        print(chr(ord('A') + j), end=" ")
+        print(chr(ord('1') + j), end=" ")
     print()
