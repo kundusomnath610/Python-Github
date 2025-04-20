7bdd7947a2e39c6d865e7c671baaccc0a80316af
Somnath Kundu <kundusomnath610@gmail.com>
2025-04-20 15:52:01 +0530

    commit

--- List_DataStructure/List_Com.py
+++ List_DataStructure/List_Com.py
@@ -12,3 +12,13 @@ print([i + 5 for i in li])
 
 li1 = [1, 2, 3, 4, 5]
 print([i for i in li1 if i%2 == 0])
+print()
+
+'''
+    This is the bad idea to create nested list..
+    cause when i access the list id it print same..
+'''
+
+list = [[]] * 5
+for list1 in list:
+    print(id(list1))
