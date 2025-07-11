a2b88099439c540e74e9c10b80b1069d14132db5
Somnath Kundu <kundusomnath610@gmail.com>
2025-07-11 15:44:48 +0530

    initial commit 11 July

--- Recursion/rev_string.py
+++ Recursion/rev_string.py
@@ -1,11 +1,12 @@
 """
     Reverse a String like hello... Using Iterative Method
 """
+str = "somnath"
 
-def rev_string(str):
-    rev_string = 0
+def reverse_string(str):
+    rev_string = ""
     for char in str:
-        rev_string += char + rev_string
+        rev_string = char + rev_string
     return rev_string
 
-print(rev_string(str))
+print(reverse_string(str))
