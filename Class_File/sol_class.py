7c3cf4faf63d0caa40a9a850a9831d740c30b0da
Somnath Kundu <kundusomnath610@gmail.com>
2025-07-26 22:04:57 +0530

    commit

--- Class_File/sol_class.py
+++ Class_File/sol_class.py
@@ -6,15 +6,20 @@ class Car:
     def new_car(self): # This is a Method
         return f"Model: {self.model}, Brand: {self.brand}"
 
-    def fuel_type(self):
+    """
+        Polymorphism for fuel_type method function in Car Class
+    """
+    def fuel_type(self): # Polymorphism for Car Class
         return "Diesel and Petrol"
 
-class ElectricCar(Car):
+class ElectricCar(Car): # Inheriting the Car class Property
     def __init__(self, brand, model, battery_size):
         super().__init__(brand, model)
         self.battery_size = battery_size
-
-    def fuel_type(self):
+    """
+        Polymorphism for fuel_type method function in ElectricCar Class
+    """
+    def fuel_type(self): # Polymorphism
         return "Electric Charge"
 
 new_car = ElectricCar("Tesla", "Model X", "5.0 Kv")
