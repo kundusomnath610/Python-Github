45f0943c499c419a08a296188c3bf47990d80916
Somnath Kundu <kundusomnath610@gmail.com>
2025-07-26 21:49:59 +0530

    commit

--- Class_File/sol_class.py
+++ Class_File/sol_class.py
@@ -6,10 +6,16 @@ class Car:
     def new_car(self): # This is a Method
         return f"Model: {self.model}, Brand: {self.brand}"
 
+    def fuel_type(self):
+        return "Diesel and Petrol"
+
 class ElectricCar(Car):
     def __init__(self, brand, model, battery_size):
         super().__init__(brand, model)
         self.battery_size = battery_size
+
+    def fuel_type(self):
+        return "Electric Charge"
 
 new_car = ElectricCar("Tesla", "Model X", "5.0 Kv")
 print(new_car.model)
